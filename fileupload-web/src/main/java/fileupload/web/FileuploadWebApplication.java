e1e7345012d7dc79e6336b84cf0f4c186e99c67b
dentaira <36123374+dentaira@users.noreply.github.com>
2020-10-22 01:01:44 +0900

    add fileupload-web project

---
+++ fileupload-web/src/main/java/fileupload/web/FileuploadWebApplication.java
@@ -0,0 +1,13 @@
+package fileupload.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileuploadWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileuploadWebApplication.class, args);
+    }
+
+}
---
+++ fileupload-web/src/test/java/fileupload/web/FileuploadWebApplicationTests.java
@@ -0,0 +1,13 @@
+package fileupload.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileuploadWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
