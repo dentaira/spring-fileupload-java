3ff9b85bcd94f681b1cc309ee636b7ec7c0a2dbe
dentaira <36123374+dentaira@users.noreply.github.com>
2021-01-18 06:40:09 +0900

    add ci settings (#6)
    * add ci settings
    
    modify pom.xml
    add schema.sql
    
    * remove schema.sql in main

--- fileupload-web/src/test/resources/schema.sql
+++ fileupload-web/src/test/resources/schema.sql
@@ -1,5 +1,5 @@
--- TODO enumを作成するときにif　existsする方法を見つける
---CREATE TYPE file_type AS ENUM ('FILE', 'DIRECTORY');
+CREATE TYPE file_type AS ENUM ('FILE', 'DIRECTORY');
+
 CREATE TABLE IF NOT EXISTS FILE (
     id CHAR(36),
     name TEXT,
