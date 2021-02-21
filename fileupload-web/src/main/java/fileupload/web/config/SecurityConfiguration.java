883dacbf23e0bd67d5f816f0b796ff0903677a9b
dentaira <36123374+dentaira@users.noreply.github.com>
2021-02-22 04:14:48 +0900

    add in-memory-authentication

--- fileupload-web/src/main/java/fileupload/web/config/SecurityConfiguration.java
+++ fileupload-web/src/main/java/fileupload/web/config/SecurityConfiguration.java
@@ -1,17 +1,34 @@
 package fileupload.web.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
-        http.csrf().disable();
+        http.authorizeRequests().anyRequest().authenticated()
+                .and().formLogin().permitAll().defaultSuccessUrl("/file/home");
     }
 }
