d7cfc4044b5063c9a9a2a941da9da178af86b609
dentaira <36123374+dentaira@users.noreply.github.com>
2021-03-13 19:11:28 +0900

    add schema.sql
    github-actions に必要だったので再作成。
    user_account テーブルを追加。

---
+++ fileupload-web/src/test/resources/schema.sql
@@ -0,0 +1,17 @@
+CREATE TYPE file_type AS ENUM ('FILE', 'DIRECTORY');
+
+CREATE TABLE IF NOT EXISTS file (
+    id CHAR(36),
+    name TEXT,
+    path TEXT,
+    type FILE_TYPE,
+    content BYTEA,
+    size BIGINT
+);
+
+CREATE TABLE IF NOT EXISTS user_account (
+    email VARCHAR(254) PRIMARY KEY,
+    name VARCHAR(64),
+    password VARCHAR(256),
+    role VARCHAR(20)
+)
