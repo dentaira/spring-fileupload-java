8b0f85f3d9c29a5a30badcdc2ce25639c424c247
dentaira <heismanreport.05@gmail.com>
2021-04-21 03:52:33 +0900

    add Header

--- cobaco-client-vue/src/App.vue
+++ cobaco-client-vue/src/App.vue
@@ -5,13 +5,52 @@
   </div>
 </template>
 
-<style lang="scss"></style>
+<style>
+html, body {
+    font-size: 16px;
+    font-family: sans-serif;
+    color: #393939;
+}
+body, div, p, h1, h2, h3, h4, ul, figure {
+    margin: 0;
+    padding: 0;
+}
+p, td, th, li {
+    line-height: 1.8;
+}
+img {
+    width: 100%;
+    height: auto;
+}
+a {
+    text-decoration: none;
+    color: #1864b9;
+}
+a:hover {
+    color: #1864b9;
+}
+.img-responsive {
+    display: block;
+    max-width: 100%;
+    height: auto;
+}
+
+.float-clear {
+    overflow: hidden;
+}
+
+.container {
+    margin: 0 auto;
+    max-width: 1000px;
+}
+</style>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HeaderComponent from '@/components/Header.vue'
 
 export default defineComponent({
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   setup() {
     
   },
---
+++ cobaco-client-vue/src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <div class="container">
+      <div class="header-inner">
+        <h1 class="header-logo">
+          <a href="#"><img src="@/assets/logo.png" alt="Cobaco"></a>
+        </h1>
+        <a href="/logout">ログアウト</a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style>
+.header-inner {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.header-logo {
+    margin: 0;
+    padding: 10px 0;
+    width: 180px;
+}
+</style>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  setup() {},
+});
+</script>
