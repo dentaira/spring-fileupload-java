619c73757dca38b83cc2f1a41c6ed82a26dc7c85
dentaira <heismanreport.05@gmail.com>
2021-04-21 04:18:37 +0900

    add breadcrumd mock

--- cobaco-client-vue/src/views/Home.vue
+++ cobaco-client-vue/src/views/Home.vue
@@ -1,12 +1,50 @@
 <template>
-  <div>
-    <h1>ホーム</h1>
-  </div>
+  <section>
+    <div class="container">
+      <div class="breadcrumb">
+        <ol>
+          <li>
+            <a href="/file/home">home</a>
+          </li>
+          <li>
+            <a href="/file/home/folderId" text="祖先ディレクトリ"></a>
+          </li>
+          <li>
+            <span>カレントディレクトリ</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<style>
+/* パンくずリスト */
+.breadcrumb {
+    min-height: 80px;
+    margin-bottom: 20px;
+}
+.breadcrumb ol {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+}
+.breadcrumb li {
+    font-size: 1.4rem;
+    display: inline;
+}
+.breadcrumb li::after {
+    content: "/";
+    color: #999;
+    margin: 0 7px;
+}
+.breadcrumb li:last-child::after {
+    content: none;
+}
+</style>
 
-export default defineComponent({
-})
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
 </script>
