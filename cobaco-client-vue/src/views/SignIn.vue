43ba47e692b0bdcc8f48401f994b201c4427f7e5
dentaira <heismanreport.05@gmail.com>
2021-04-23 03:27:54 +0900

    refactor SignIn page

--- cobaco-client-vue/src/views/SignIn.vue
+++ cobaco-client-vue/src/views/SignIn.vue
@@ -1,12 +1,10 @@
 <template>
   <section class="signInArea">
     <form class="signInForm">
-      <div class="signInFormInner">
-        <img src="@/assets/logo.png" class="logo" />
-        <input v-on="username" placeholder="Username" />
-        <input v-on="password" placeholder="Password" />
-        <button class="button" href="#" @click="signIn">Sign in</button>
-      </div>
+      <img src="@/assets/logo.png" class="logo" alt="Cobaco" />
+      <input v-on="username" placeholder="Username" />
+      <input v-on="password" placeholder="Password" />
+      <button class="button" href="#" @click="signIn">Sign in</button>
     </form>
   </section>
 </template>
@@ -15,20 +13,21 @@
 .signInArea {
   margin: 100px auto;
 }
-section .signInFormInner {
+.signInForm {
   display: flex;
   flex-flow: column;
   margin: auto;
   width: 300px;
 }
-.logo {
-  width: 210px;
-}
-.sigInForm, input {
+.sigInForm,
+input {
   width: 100%;
   height: 40px;
   margin: 10px auto;
   outline: none;
+}
+.logo {
+  width: 210px;
 }
 .button {
   display: inline-block;
@@ -43,7 +42,8 @@ section .signInFormInner {
   text-align: center;
   text-decoration: none;
 }
-.button:hover, .button:focus {
+.button:hover,
+.button:focus {
   opacity: 0.8;
   outline: none;
 }
