ae9a915c625a3e0286fb4c7a255dddfb832e85ce
dentaira <36123374+dentaira@users.noreply.github.com>
2021-04-23 03:39:48 +0900

    Add vue client (#14)
    * Initial vue-client
    
    * setting linter and formatter
    
    * remove unused files
    
    * add empty view
    
    * add Header
    
    * add normalize.css
    
    * add breadcrumd mock
    
    * add file list mock
    
    * add SignIn
    
    * add SignIn page
    
    * refactor SignIn page
    
    * modify SignIn page

---
+++ cobaco-client-vue/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view />
+  </div>
+</template>
+
+<style>
+html, body {
+    font-size: 16px;
+    font-family: sans-serif;
+    color: #393939;
+}
+*, *::before, *::after {
+    box-sizing: border-box;
+}
+body, div, p, h1, h2, h3, h4, ul, figure {
+    margin: 0;
+    padding: 0;
+}
+p, td, th, li {
+    line-height: 1.8;
+}
+img {
+    width: 100%;
+    height: auto;
+}
+a {
+    text-decoration: none;
+    color: #1864b9;
+}
+a:hover {
+    color: #1864b9;
+}
+.img-responsive {
+    display: block;
+    max-width: 100%;
+    height: auto;
+}
+
+.float-clear {
+    overflow: hidden;
+}
+
+.container {
+    margin: 0 auto;
+    max-width: 1000px;
+}
+</style>
+
+<script lang="ts">
+import 'normalize.css'
+import { defineComponent } from 'vue'
+import HeaderComponent from '@/components/Header.vue'
+
+export default defineComponent({
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  setup() {
+    
+  },
+  components: {
+    Header: HeaderComponent,
+  }
+})
+</script>
